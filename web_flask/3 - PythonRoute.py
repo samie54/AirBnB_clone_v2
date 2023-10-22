db7a5de06c4b074dfb086f523f0949fb362a9cf3
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-22 13:09:21 -0700

    3 - PythonRoute.py

--- web_flask/3 - PythonRoute.py
+++ web_flask/3 - PythonRoute.py
@@ -3,38 +3,39 @@
 Flask web application
 @authors: Samuel Atiemo & Mahmoud Khairi.
 """
+
 from flask import Flask
 
+
 app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_hbnb():
-    """ Print Web """
+def index():
+    '''The home page.'''
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ Print Web """
+    '''The hbnb page.'''
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_is_fun(text):
-    """ Print a char C followed by the value of the text variable """
+def c_page(text):
+    '''The c page.'''
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python')
 @app.route('/python/<text>')
-def python_is_cool(text='is cool'):
-    """ Print Python, followed by the value of the text variable,
-    with default value of text: is cool"""
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+    '''The python page.'''
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
