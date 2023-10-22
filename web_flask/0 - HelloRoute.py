b7fd73dfdacfed4803d47f7975858387355f4456
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-22 13:17:36 -0700

    0 - HelloRoute.py

--- web_flask/0 - HelloRoute.py
+++ web_flask/0 - HelloRoute.py
@@ -5,13 +5,14 @@ Flask web application
 """
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
